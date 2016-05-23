39e0d4ec39fc20acb086d09fa02f3b4ffe5d0bef
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2016-05-23 02:32:52 -0500

    config
    cambio en la configuracion

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var nodemailer = require('nodemailer')
-	,debug = require('debug')('util:mailer')
-	,config = require('../config');
-
-var transporter = nodemailer.createTransport(config.mailer);
-debug('Listo para enviar correos');
+	,debug = require('debug')('util:mailer');
 
 var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 
@@ -23,7 +19,13 @@ var sendMail = function (from, to, subject, body, options, cb) {
 	});
 }
 
-module.exports = {
-	checkEmail: pattern,
-	sendMail: sendMail
+function init(config) {
+  var transporter = nodemailer.createTransport(config);
+  debug('Listo para enviar correos');
+  return {
+    checkEmail: pattern,
+    sendMail: sendMail
+  };
 }
+
+module.exports = init;
