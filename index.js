b5f39c6d5cfd830294bdefad36931400716946fd
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2016-05-23 02:48:30 -0500

    sendMail
    detalle resuelto

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ var nodemailer = require('nodemailer')
 
 var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 
-var sendMail = function (from, to, subject, body, options, cb) {
-	var mailOp = {
-		from: from,
-		to: to,
-		subject: subject,
-		html: body
-	}
-	if(options.attachments) mailOp.attachments = options.attachments;
-	if(options.attachments) mailOp.attachments = options.attachments;
-	transporter.sendMail(mailOp, function(error, info){
-		debug(error);
-		debug(info);
-		cb(error,info);
-	});
+var sendMail = function (transporter) {
+  return function (from, to, subject, body, options, cb) {
+    var mailOp = {
+      from: from,
+      to: to,
+      subject: subject,
+      html: body
+    }
+    if(options.attachments) mailOp.attachments = options.attachments;
+    if(options.attachments) mailOp.attachments = options.attachments;
+    transporter.sendMail(mailOp, function(error, info){
+      debug(error);
+      debug(info);
+      cb(error,info);
+    });
+  }
 }
 
 function init(config) {
@@ -24,7 +26,7 @@ function init(config) {
   debug('Listo para enviar correos');
   return {
     checkEmail: pattern,
-    sendMail: sendMail
+    sendMail: sendMail(transporter)
   };
 }
 
