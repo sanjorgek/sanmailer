4b12d44d95581f1068287f886823bb41f7b96cec
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2016-06-09 00:54:10 -0500

    Added email module

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sanmailer
-Nodemailer adapter
+[nodemailer](https://www.npmjs.com/package/nodemailer) and [email](https://www.npmjs.com/package/email) simple adapter
 
   [![NPM Version][npm-image]][npm-url]
   [![NPM Downloads][downloads-image]][downloads-url]
@@ -8,8 +8,12 @@ Nodemailer adapter
 ~~~bash
 $ npm install sanmailer 
 ~~~
+If you have [Sendmail](http://www.sendmail.com/sm/open_source/) installed you don't need settings
+~~~js
+var mailer = require('sanmailer').local();
+~~~
 
-You need a configuration
+With host settings:
 ~~~js
 var mailerConf = {
   host: 'some.valid.host.com',
@@ -20,10 +24,8 @@ var mailerConf = {
     pass: 'password'
   }  
 };
-~~~
-Then you create a mailer
-~~~js
-var mailer = require('sanmailer')(mailerConf);
+...
+var mailer = require('sanmailer').host(mailerConf);
 ~~~
 
 ## Use
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var nodemailer = require('nodemailer')
+  ,Email = require('email').Email
 	,debug = require('debug')('sanmailer');
 
 var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
@@ -12,7 +13,7 @@ var sendMail = function (transporter) {
       html: body
     }
     if(options.attachments) mailOp.attachments = options.attachments;
-    if(options.attachments) mailOp.attachments = options.attachments;
+    if(options.replyTo) mailOp.replyTo = options.replyTo;
     transporter.sendMail(mailOp, function(error, info){
       debug(error);
       debug(info);
@@ -21,7 +22,7 @@ var sendMail = function (transporter) {
   }
 }
 
-function init(config) {
+function initHost(config) {
   var transporter = nodemailer.createTransport(config);
   debug('Listo para enviar correos');
   return {
@@ -30,4 +31,24 @@ function init(config) {
   };
 }
 
-module.exports = init;
+function local() {
+  return {
+    checkEmail: pattern.test,
+    sendMail: function(from, to, subject, body, options, cb) {
+      new Email({
+        from: from,
+        to: to,
+        subject: subject,
+        body: body
+      }).send(function (err) {
+        debug(err);
+        cb(err);
+      });
+    }
+  }
+}
+
+module.exports = {
+  host: initHost,
+  local: local  
+};
