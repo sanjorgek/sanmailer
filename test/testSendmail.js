d3ba10e51ca620bbffc1d42305facf4f7b53b6fe
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2016-06-15 02:22:03 -0500

    test with actual settings

---
+++ test/testSendmail.js
@@ -0,0 +1,103 @@
+var sanmailer;
+
+
+describe('sanmailer in local mode', function() {
+  before(function() {
+    sanmailer = require('../index').local();
+  });
+  
+  describe('sendMail', function () {
+    it("empty from email", function (done) {
+      sanmailer.sendMail(
+        '',
+        'to@mail.com',
+        'Subject',
+        'body',
+        {},
+        function(err, info) {
+          done(err.message!='node-email error: from is required');
+        }
+      );
+    });
+    
+    it("bad from email", function (done) {
+        sanmailer.sendMail(
+          'fefrefger',
+          'to@mail.com',
+          'Subject',
+          'body',
+          {},
+          function(err, info) {
+            done(err.message!='node-email error: invalid email address : fefrefger');
+          }
+        );
+    });
+    
+    it("empty to email", function (done) {
+      sanmailer.sendMail(
+        'fefrefger@mail.com',
+        '',
+        'Subject',
+        'body',
+        {},
+        function(err, info) {
+          done(err.message!='node-email error: to is required');
+        }
+      );
+    });
+    
+    it("bad to email", function (done) {
+      sanmailer.sendMail(
+        'fefrefger@mail.com',
+        '@mail.com',
+        'Subject',
+        'body',
+        {},
+        function(err, info) {
+          done(err.message!='node-email error: invalid email address : @mail.com');
+        }
+      );
+    });
+    
+    it("bad subject", function (done) {
+      sanmailer.sendMail(
+        'fefrefger@mail.com',
+        'dvverw@mail.com',
+        '',
+        'body',
+        {},
+        function(err, info) {
+          done(err.message!='node-email error: subject is required');
+        }
+      );
+    });
+    
+    it("OK", function (done) {
+      sanmailer.sendMail(
+        'fefrefger@mail.com',
+        'dvverw@mail.com',
+        'subject',
+        'wfwrgww',
+        {},
+        function(err, info) {
+          console.log(err.message);
+          done(err.message!='node-email error: subject is required');
+        }
+      );
+    });
+    
+    it("OK", function (done) {
+      sanmailer.sendMail(
+        'fefrefger@mail.com',
+        'dvverw@mail.com',
+        'subject',
+        '',
+        {},
+        function(err, info) {
+          console.log(err.message);
+          done(err.message!='node-email error: subject is required');
+        }
+      );
+    });
+  });
+});
